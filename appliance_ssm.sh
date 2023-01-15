2d60e952ee4a3daef4568c40666d4fe6cf8f42b6
whchoi98 <whchoi98@gmail.com>
2023-01-16 01:21:59 +0900

    add file - ssm shell

---
+++ appliance_ssm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# command ./appliacne_ssm.sh
+
+aws ec2 describe-instances --filters 'Name=tag:Name,Values=GWLBVPC-Appliance-10.254.11.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances --filters 'Name=tag:Name,Values=GWLBVPC-Appliance-10.254.11.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances --filters 'Name=tag:Name,Values=GWLBVPC-Appliance-10.254.12.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances --filters 'Name=tag:Name,Values=GWLBVPC-Appliance-10.254.12.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+export Appliance_11_101=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=GWLBVPC-Appliance-10.254.11.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+export Appliance_11_102=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=GWLBVPC-Appliance-10.254.11.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+export Appliance_12_101=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=GWLBVPC-Appliance-10.254.12.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+export Appliance_12_102=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=GWLBVPC-Appliance-10.254.12.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+echo "export Appliance_11_101=${Appliance_11_101}"| tee -a ~/.bash_profile
+echo "export Appliance_11_102=${Appliance_11_102}"| tee -a ~/.bash_profile
+echo "export Appliance_12_101=${Appliance_12_101}"| tee -a ~/.bash_profile
+echo "export Appliance_12_102=${Appliance_12_102}"| tee -a ~/.bash_profile
